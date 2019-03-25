e4be300218c0b698da0b1e36f37470db5c2efd41
David Stranders <dstranders@vxcompany.com>
2019-03-25 19:18:08 +0100

    Initial commit

---
+++ src/main/java/com/vxcompany/talentlab/dthschatbotapi/DthsChatbotApiApplication.java
@@ -0,0 +1,13 @@
+package com.vxcompany.talentlab.dthschatbotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DthsChatbotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DthsChatbotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vxcompany/talentlab/dthschatbotapi/DthsChatbotApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vxcompany.talentlab.dthschatbotapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DthsChatbotApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
